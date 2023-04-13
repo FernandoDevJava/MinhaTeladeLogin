8c276a9cb1adba8d51a4594e45d153f1dd1cc6c2
Fernando Machado <107931700+FernandoDevJava@users.noreply.github.com>
2023-04-13 08:56:04 -0300

    Add files via upload

--- app/src/main/java/com/nubank/login/http/HttpHelper.kt
+++ app/src/main/java/com/nubank/login/http/HttpHelper.kt
@@ -1,27 +1,55 @@
 package com.nubank.login.http
 
+import okhttp3.MediaType
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.RequestBody.Companion.toRequestBody
-import okhttp3.logging.HttpLoggingInterceptor
-import java.util.concurrent.TimeUnit
+import okhttp3.Response
+import java.net.URL
 
 class HttpHelper {
 
-    fun post(json: String): String {
+    fun post(json: String) {
 
-        var cliente = OkHttpClient.Builder()
-            .connectTimeout(60, TimeUnit.SECONDS)
-            .readTimeout(60, TimeUnit.SECONDS)
-            .addInterceptor(HttpLoggingInterceptor().apply {
-                level = HttpLoggingInterceptor.Level.BODY
-            })
+        val client = OkHttpClient().newBuilder()
+            .build()
+        val headerHttp = "application/json; charset=utf-8".toMediaTypeOrNull()
+        val body = json.toRequestBody(headerHttp)
+        val URL = "http://home.lsfcloud.com.br:8080/api/auth"
+        val request: Request = Request.Builder().url(URL).post(body)
+            .url("http://home.lsfcloud.com.br:8080/api/auth")
+            .method("POST", body)
+            .addHeader("Content-Type", "application/json")
+            .build()
+        val response: Response = client.newCall(request).execute()
+
+
+        //conexões
+        // ec2-15-229-8-134.sa-east-1.compute.amazonaws.com
+        // aws.lsfcloud.com.br:8080/api/auth/login
+
+
+        /*val client = OkHttpClient()
+
+        val request = Request.Builder()
+            .url("http://ec2-15-229-8-134.sa-east-1.compute.amazonaws.com:8080/api/auth")
             .build()
 
+        client.newCall(request).execute().use { response ->
+            if (!response.isSuccessful) throw IOException("Unexpected code $response")
 
-        // Definir URL do servidor
-        val URL = "https://gestao-tarefas.onrender.com/auth"
+            for ((name, value) in response.headers) {
+                println("$name: $value")
+            }
+
+            Log.d("teste", response.body!!.string())*/
+
+
+        /////////////////////////////////////////////////////////////////////////////
+
+        /* // Definir URL do servidor
+        val URL = "http://ec2-15-229-8-134.sa-east-1.compute.amazonaws.com:8080/api/auth"
 
         // Definir o cabeçalho
         val headerHttp = "application/json; charset=utf-8".toMediaTypeOrNull()
@@ -33,38 +61,23 @@ class HttpHelper {
         val body = json.toRequestBody(headerHttp)
 
         // Criar a requisição http para o servidor
-        var request = Request.Builder().url(URL).post(body).build()
+        val request = Request.Builder().url(URL).post(body).build()
 
         // Utilizar o client para fazer a requisição e receber a resposta
         val response = client.newCall(request).execute()
 
-        return response.body.toString()
+        return response.body.toString()*/
 
-
-        /*
-
-        //conexões
-        ec2-15-229-8-134.sa-east-1.compute.amazonaws.com
-
-        aws.lsfcloud.com.br
-
-
-        val client = OkHttpClient()
-
-        val request = Request.Builder()
-            .url("https://ec2-15-229-8-134.sa-east-1.compute.amazonaws.com:8080/api/auth/cadastro")
-            .build()
-
-        client.newCall(request).execute().use { response ->
-            if (!response.isSuccessful) throw IOException("Unexpected code $response")
-
-            for ((name, value) in response.headers) {
-                println("$name: $value")
-            }
-
-            Log.d("teste", response.body!!.string())
-         */
+        /*Log.d("teste", response.body!!.string())*/
 
     }
 
+    private fun URL(mediaType: MediaType?, s: String): URL {
+        TODO("Not yet implemented")
+    }
+
+}
+
+private fun Any.method(method: String, body: URL): Request.Builder {
+    TODO("Not yet implemented")
 }
