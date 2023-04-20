12bd67bfae1dfbe807c20a5859307c5c3f4a766b
fernandodevjava <fernando-pgt@outlook.com>
2023-04-20 10:09:57 -0300

    Commit

--- app/src/main/java/com/nubank/login/MainActivity.kt
+++ app/src/main/java/com/nubank/login/MainActivity.kt
@@ -89,9 +89,11 @@ class MainActivity : AppCompatActivity() {
                 ProgressBarUtils.close(context)
                 if (response.first != "erro") {
                     if (response.first == "200") {
+                        Util.menssagemToast(context, context.getString(R.string.login_sucesso))
                         binding.idButtonLogin.setOnClickListener {
                             var exemplo = Intent(applicationContext, Aplicacao::class.java)
                             startActivity(exemplo)
+                            finish()
                         }
                     } else {
                         var respostaErro = response.second
